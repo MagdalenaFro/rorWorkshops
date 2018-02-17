525eda11069a9b91178b40b1dfb51ef8e294de7b
Magdalena <magdalena.fro@gmail.com>
2018-02-17 15:09:00 +0100

    Initial commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,6 @@
+class UsersController < ApplicationController #dziedziczenie po klasie ApplicationController
+    # bedzie szukal w views/users/index.erb bo
+    # bierze sobie domyslnie nazwe pliku
+    # taka jak zdefinowana w metodzie
+    def index ;end
+end
---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+ 
+  get '/test', to: proc { |el| [200, {}, ["ok"]]}
+
+  #po podaniu sciezki users odwola sie kontrolera wiec bedzie
+  # szukal nazwy users_controller w folderze app/controllers
+  # oraz 
+  get '/users', to: "users#index"
+
+  get '/contacts', to: "contacts#index"
+
+end
