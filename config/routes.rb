92066657a5460aafd2acb53a6e1449d4d5f34cbd
Magdalena <magdalena.fro@gmail.com>
2018-02-17 15:26:02 +0100

    Add static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
  
-  get '/test', to: proc { |el| [200, {}, ["ok"]]}
+  get "/test", to: proc { |el| [200, {}, ["ok"]]}
 
   #po podaniu sciezki users odwola sie kontrolera wiec bedzie
   # szukal nazwy users_controller w folderze app/controllers
   # oraz 
-  get '/users', to: "users#index"
+  get "/users", to: "users#index"
 
-  get '/contacts', to: "contacts#index"
+  get "/contacts", to: "static_pages#contacts"
+
+  get "/about", to: "static_pages#about"
 
 end
